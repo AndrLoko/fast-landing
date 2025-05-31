d5761b18efda45bd41ae429e8008d5c4746aa116
Andrew Lokotosh <andrewlokoffical@gmail.com>
2025-05-31 23:44:42 +0200

    Checkout branch and start section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Navigation from "@/widgets/Navigation/navigation";
 import Header from "@/widgets/Header/header";
+import WhatWeDo from "@/widgets/WhatWeDo/whatWeDo";
 
 export default function Home() {
   return (
-    <header>
-      <div>
-        <Navigation />
-        <Header />
-      </div>
-    </header>
+    <>
+      <header>
+          <Navigation />
+          <Header />
+      </header>
+      <main>
+        <WhatWeDo />
+      </main>
+    </>
   );
 }
--- src/widgets/WhatWeDo/whatWeDo.tsx
+++ src/widgets/WhatWeDo/whatWeDo.tsx
@@ -3,7 +3,18 @@ import React from 'react';
 const whatWeDo = () => {
     return (
         <div>
-            
+            <h2>What we do?</h2>
+            <div>
+                <p>YouTube Highlights</p>
+                <p>longform → shortform</p>
+                <p>Editing packages for recurring clients</p>
+                <p>Content ideation & creative support</p>
+                <p>UGC-style ads</p>
+                <p>Thumbnail & title optimization</p>
+            </div>
+            <div>
+                {/* SLIDER */}
+            </div>
         </div>
     );
 };
